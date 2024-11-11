ce7558ca663c440fbca9d2cf9ce9f3a3f78458e8
Maxim <maximozaitsev@gmail.com>
2024-11-11 18:47:10 +0400

    fix: logo image

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Logo = ({ onClick }: { onClick: () => void }) => {
   return (
     <div onClick={onClick} style={{ cursor: "pointer" }}>
-      <Image src="/logo.svg" alt="Logo" width={128} height={48} priority />
+      <Image src="/logo.svg" alt="Logo" width={225} height={41} priority />
     </div>
   );
 };
