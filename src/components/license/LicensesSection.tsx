ae9c0d24d9117656f7b848e14ac38f6a2befd2d7
Maxim <maximozaitsev@gmail.com>
2025-02-14 17:04:58 +0400

    deploy: kangaroo88

--- src/components/license/LicensesSection.tsx
+++ src/components/license/LicensesSection.tsx
@@ -34,17 +34,17 @@ export default function LicensesSection() {
                 <BlockRenderer key={index} block={block} />
               ))}
             </div>
-            {licenses.showImage && (
-              <div className={styles.rightImage}>
-                <Image
-                  src={licenceImage}
-                  alt="Licenses"
-                  width={240}
-                  height={240}
-                  quality={100}
-                />
-              </div>
-            )}
+            {/* {licenses.showImage && (
+              // <div className={styles.rightImage}>
+              //   <Image
+              //     src={licenceImage}
+              //     alt="Licenses"
+              //     width={240}
+              //     height={240}
+              //     quality={100}
+              //   />
+              // </div>
+            )} */}
           </div>
         )}
 
