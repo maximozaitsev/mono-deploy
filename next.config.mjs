0811c790f194f4946a25fbc035846da58d98b340
Maxim <maximozaitsev@gmail.com>
2025-05-16 20:50:32 +0400

    deploy: gbets

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 
 import nextPWA from "@ducanh2912/next-pwa";
-const url = "kwiff-online.com";
+const url = "gbets-online.com";
 
 const withPWA = nextPWA({
   dest: "public",
