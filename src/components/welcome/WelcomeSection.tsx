2b88d09f2f93b5971c7387bffd0cc24329630858
Maxim <maximozaitsev@gmail.com>
2025-08-18 15:23:09 +0300

    feat: add fetch priority high

--- src/components/welcome/WelcomeSection.tsx
+++ src/components/welcome/WelcomeSection.tsx
@@ -40,7 +40,7 @@ export default function WelcomeSection() {
           alt="Welcome Mobile"
           loading="eager"
           decoding="async"
-          fetchpriority="high"
+          fetchPriority="high"
         />
       </figure>
 
