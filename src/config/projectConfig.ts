d827a976834883af1e5bf696eb884b3e3246c37e
Maxim <maximozaitsev@gmail.com>
2025-08-21 12:58:57 +0300

    deploy: bet blast

--- src/config/projectConfig.ts
+++ src/config/projectConfig.ts
@@ -1,8 +1,8 @@
 import languagesJson from "../../public/content/languages.json";
 import { getProjectGeoForLang } from "../utils/localeMap";
 
-export const PROJECT_NAME = "Gambloria Casino";
-export const PROJECT_URL = "gambloria-online.com";
+export const PROJECT_NAME = "BetBlast Casino";
+export const PROJECT_URL = "bet-blast-online.com";
 
 export const PROJECT_GEO = getProjectGeoForLang(
   languagesJson.defaultLang || "au"
