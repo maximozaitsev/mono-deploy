28f875f6be9e697abe6267f85e53609023966caf
Maxim <maximozaitsev@gmail.com>
2025-08-22 22:13:05 +0300

    feat: try font optimization x2

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 
 import nextPWA from "@ducanh2912/next-pwa";
-const url = "oshi-casino.net";
+const url = "url";
 
 const withPWA = nextPWA({
   dest: "public",
--- src/config/projectConfig.ts
+++ src/config/projectConfig.ts
@@ -1,8 +1,8 @@
 import languagesJson from "../../public/content/languages.json";
 import { getProjectGeoForLang } from "../utils/localeMap";
 
-export const PROJECT_NAME = "Oshi Casino";
-export const PROJECT_URL = "oshi-casino.net";
+export const PROJECT_NAME = "Project Name";
+export const PROJECT_URL = "example.com";
 
 export const PROJECT_GEO = getProjectGeoForLang(
   languagesJson.defaultLang || "au"
