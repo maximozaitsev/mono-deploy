a7b897e5f10f1f50ad6edca15865301ac1338f3d
Maxim <maximozaitsev@gmail.com>
2025-11-10 16:34:16 +0300

    feat: update nbcasino-online.com - changed: src/components/top-casinos/OfferCard.module.scss, src/config/projectConfig.ts

--- src/config/projectConfig.ts
+++ src/config/projectConfig.ts
@@ -1,4 +1,4 @@
-export const PROJECT_NAME = "NB Casino";
+export const PROJECT_NAME = "New Brunswick Casino";
 export const PROJECT_GEO = "Canada";
 export const PROJECT_URL = "nbcasino-online.com";
 export const CASINO_MODE = "offline"; // "online" или "offline"
