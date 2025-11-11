dd5f9f9c387c193e23fa23df81006335d97ccc4b
Maxim <maximozaitsev@gmail.com>
2025-11-11 17:20:48 +0300

    feat: update riverrock-online.com - changed: src/config/projectConfig.ts

--- src/config/projectConfig.ts
+++ src/config/projectConfig.ts
@@ -1,4 +1,4 @@
-export const PROJECT_NAME = "New Brunswick Casino";
+export const PROJECT_NAME = "River Rock Casino";
 export const PROJECT_GEO = "Canada";
 export const PROJECT_URL = "riverrock-online.com";
 export const CASINO_MODE = "offline"; // "online" или "offline"
