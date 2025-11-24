91897f97edb228ed6092b212b192c9c4925e9f4e
Maxim <maximozaitsev@gmail.com>
2025-11-24 17:51:23 +0300

    feat: update grandvilla-casino.com - changed: src/components/welcome/WelcomeSection.module.scss, src/config/projectConfig.ts

--- src/config/projectConfig.ts
+++ src/config/projectConfig.ts
@@ -1,4 +1,4 @@
-export const PROJECT_NAME = "Grand Villa Windsor";
+export const PROJECT_NAME = "Grand Villa Casino";
 export const PROJECT_GEO = "Canada";
 export const PROJECT_URL = "grandvilla-casino.com";
 export const CASINO_MODE = "offline"; 
